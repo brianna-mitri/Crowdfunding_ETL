4bb3b8b0bbfb53fbe970c594967a73cb8ba99c20
brianna-mitri <brianna.mitri@gmail.com>
2025-01-06 20:54:15 -0800

    Updated sql schema

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,49 +1,44 @@
---create table Category
-CREATE TABLE Category (
-    category_id CHAR(7) PRIMARY KEY,
-    category_name VARCHAR(50) NOT NULL
+--create table category
+CREATE TABLE category (
+    category_id VARCHAR(10) PRIMARY KEY,
+    category_name VARCHAR(20) NOT NULL
 );
 
-select * from category;
 
-
---create table SubCategory
-CREATE TABLE SubCategory (
-    subcategory_id CHAR(10) PRIMARY KEY,
-    subcategory_name VARCHAR(50) NOT NULL
+--create table subcategory
+CREATE TABLE subcategory (
+    subcategory_id VARCHAR(10) PRIMARY KEY,
+    subcategory_name VARCHAR(20) NOT NULL
 );
 
-select * from SubCategory;
 
-CREATE TABLE Contacts (
+--create table contacts
+CREATE TABLE contacts (
     contact_id INT PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE
 );
-select * from contacts;
 
-CREATE TABLE Campaign (
+
+--create table campaign
+CREATE TABLE campaign (
     cf_id INT PRIMARY KEY,
     contact_id INT NOT NULL,
-    company_name VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
+    company_name VARCHAR(50) NOT NULL,
+    description VARCHAR(70) NOT NULL,
     goal FLOAT NOT NULL,
     pledged FLOAT NOT NULL,
-    outcome TEXT NOT NULL  
+    outcome VARCHAR(10) NOT NULL  
 		CHECK (outcome IN ('failed', 'successful', 'live', 'canceled')),
     backers_count INT NOT NULL,
     country VARCHAR(2) NOT NULL,
     currency VARCHAR(3) NOT NULL,
-    launch_date TIMESTAMP NOT NULL,
-    end_date TIMESTAMP NOT NULL,
+    launch_date DATE NOT NULL,
+    end_date DATE NOT NULL,
     category_id VARCHAR(10) NOT NULL,
     subcategory_id VARCHAR(10) NOT NULL,
     FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
     FOREIGN KEY (category_id) REFERENCES category(category_id),
     FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
-);
-
-
-select * from Campaign;
-    
+);   
