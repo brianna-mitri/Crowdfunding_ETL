b236aa377d7e60be2fd1be8c3c3578e9da190517
sumi.789 <110785423+raut789@users.noreply.github.com>
2025-01-06 22:57:27 -0500

    added the SQL schema file

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,48 @@
+--create table Category
+CREATE TABLE Category (
+    category_id CHAR(7) PRIMARY KEY,
+    category_name VARCHAR(50) NOT NULL
+);
+
+select * from category;
+
+
+--create table SubCategory
+CREATE TABLE SubCategory (
+    subcategory_id CHAR(10) PRIMARY KEY,
+    subcategory_name VARCHAR(50) NOT NULL
+);
+
+select * from SubCategory;
+
+CREATE TABLE Contacts (
+    contact_id INT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
+select * from contacts;
+
+CREATE TABLE Campaign (
+    cf_id INT NOT NULL,
+    contact_id INT PRIMARY KEY,
+    company_name VARCHAR(255) NOT NULL,
+    description TEXT,
+    goal DECIMAL(12, 2) NOT NULL,
+    pledged DECIMAL(12, 2),
+    outcome VARCHAR(20) NOT NULL,
+    backers_count INT NOT NULL,
+    country VARCHAR(2) NOT NULL,
+    currency VARCHAR(3) NOT NULL,
+    launch_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP NOT NULL,
+    category_id VARCHAR(10) NOT NULL,
+    subcategory_id VARCHAR(10) NOT NULL,
+    FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+    FOREIGN KEY (category_id) REFERENCES category(category_id),
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+);
+
+
+select * from Campaign;
+    
